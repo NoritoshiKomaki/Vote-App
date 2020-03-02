ba734fea56fd400f51f8ec5bd6c96655a18a6659
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-03-02 23:48:14 +0900

    save

--- app/assets/javascripts/macsNew.js
+++ app/assets/javascripts/macsNew.js
@@ -1,9 +1,16 @@
 $(function(){
 
-
+  $('#mac_spec_1').change(function() {
+    $('.button').click(function() {
+      $('.que1').slideUp(300);
+      $('.que4').delay(300).hide().slideDown();
+    });
+  });
   
-  $('.button').click(function() {
-    $('.que1').slideUp(300);
-    $('.que2').delay(300).hide().slideDown();
+  $('#mac_spec_1').change(function() {
+    $('.button').click(function() {
+      $('.que1').slideUp(300);
+      $('.que4').delay(300).hide().slideDown();
+    });
   });
 });
