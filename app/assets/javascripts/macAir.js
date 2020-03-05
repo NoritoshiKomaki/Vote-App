4b908685c7a54222f2e1e74ee05ad5e975e020a7
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-03-05 16:06:25 +0900

    save

--- app/assets/javascripts/macAir.js
+++ app/assets/javascripts/macAir.js
@@ -1,17 +1,51 @@
 $(function(){
 // mac選択 
-  $('.lab1').click(function() {
-    $('.que1').slideUp(300);
-    $('.que4').delay(300).hide().slideDown();
+  $('#lab1').click(function() {
+    $('#que1').slideUp(300);
+    $('#que4').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 33%; } </style>');
   });
   // memory選択
-  $('.lab8').click(function() {
-    $('.que4').slideUp(300);
-    $('.que7').delay(300).hide().slideDown();
+  $('.lab6').click(function() {
+    $('#que4').slideUp(300);
+    $('#que7').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 66%; } </style>');
   });
   // storage選択
-  $('.lab15').click(function() {
-    $('.que7').slideUp(300);
-    $('.que10').delay(300).hide().slideDown();
+  $('.lab9').click(function() {
+    $('#que7').slideUp(300);
+    $('#que10').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 100%; } </style>');
+  });
+  // backボタン設定
+  $('.back1').click(function() {
+    $('#que2, #que3, #que4').slideUp(300);
+    $('#que1').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 0%; } </style>');
+  });
+  $('.back2').click(function() {
+    $('#que5').slideUp(300);
+    $('#que2').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 25%; } </style>');
+  });
+  $('.back3').click(function() {
+    $('#que6').slideUp(300);
+    $('#que3').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 25%; } </style>');
+  });
+  $('.back4').click(function() {
+    $('#que7').slideUp(300);
+    $('#que4').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 33%; } </style>');
+  });
+  $('.back5').click(function() {
+    $('#que8').slideUp(300);
+    $('#que5').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 55%; } </style>');
+  });
+  $('.back6').click(function() {
+    $('#que9').slideUp(300);
+    $('#que6').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 55%; } </style>');
   });
 });
--- app/assets/javascripts/macPro16.js
+++ app/assets/javascripts/macPro16.js
@@ -1,22 +1,26 @@
 $(function() {
   // mac選択 
-  $('.lab3').click(function() {
-    $('.que1').slideUp(300);
-    $('.que3').delay(300).hide().slideDown();
+  $('#lab3').click(function() {
+    $('#que1').slideUp(300);
+    $('#que3').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 25%; } </style>');
   });
 // cpu選択
-  $('.lab6').click(function() {
-    $('.que3').slideUp(300);
-    $('.que6').delay(300).hide().slideDown();
+  $('.lab5').click(function() {
+    $('#que3').slideUp(300);
+    $('#que6').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 50%; } </style>');
   });
   // memory選択
-  $('.lab12').click(function() {
-    $('.que6').slideUp(300);
-    $('.que9').delay(300).hide().slideDown();
+  $('.lab8').click(function() {
+    $('#que6').slideUp(300);
+    $('#que9').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 75%; } </style>');
   });
 // storage選択
-  $('.lab24').click(function() {
-    $('.que9').slideUp(300);
-    $('.que10').delay(300).hide().slideDown();
+  $('.lab11').click(function() {
+    $('#que9').slideUp(300);
+    $('#que10').delay(300).hide().slideDown();
+    $('head').append('<style>.select::after { width: 100%; } </style>');
   });
 });
