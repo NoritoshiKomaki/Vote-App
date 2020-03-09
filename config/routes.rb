1819e7e0670beaa76cfa244f98151c82b7d933e6
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-03-09 22:15:06 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'homes#index'
+  root :to => 'homes#login'
   get '/homes', to: 'homes#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
