e4b1f3fbd94ea55667539f4703e9e47e415b0a55
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-03-12 10:33:40 +0900

    save

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
   def create
     @like = Like.new(user_id: @current_user.id, twitter_id: params[:twitter_id])
     @like.save
-    redirect_to("/twitters/#{params[:twitter_id]}")
   end
 
   def destroy
     @like = Like.find_by(user_id: @current_user.id, twitter_id: params[:twitter_id])
     @like.destroy
-    redirect_to("/twitters/#{params[:twitter_id]}")
   end
+
+  private
+
+  def set_variables
+    @twitter = Twitter.find(params[:twitter_id])
+    @id_name = "#like-link-#{@twitter.id}"
+  end
+
 end
